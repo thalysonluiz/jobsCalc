e98c7f37e8eabab1f1024e549faf26e477644440
Thalyson Luiz <thalysonluiz@gmail.com>
2021-12-14 11:49:50 -0300

    ejs e page-header

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const routes = express.Router();
 
-const basePath = __dirname + "/views";
+const views = __dirname + "/views/";
 
-routes.get("/", (req, res) => res.sendFile(basePath + "/index.html"));
-routes.get("/job", (req, res) => res.sendFile(basePath + "/job.html"));
-routes.get("/job/edit", (req, res) =>
-  res.sendFile(basePath + "/job-edit.html")
+const profile = {
+  name: "Jakeliny",
+  avatar:
+    "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+  "monthly-budget": 3000,
+  "hours-per-day": 5,
+  "days-per-week": 5,
+  "vacation-per-year": 4,
+};
+
+routes.get("/", (req, res) => res.render(views + "index"));
+routes.get("/job", (req, res) => res.render(views + "job"));
+routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile })
 );
-routes.get("/profile", (req, res) => res.sendFile(basePath + "/profile.html"));
 
-routes.get("/index.html", (req, res) => {
+routes.get("/index", (req, res) => {
   return res.redirect("/");
 });
 
