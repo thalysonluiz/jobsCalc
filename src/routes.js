ca0f2a94a4a8916a1954892b9cb0b8deaa2cad47
Thalyson Luiz <thalysonluiz@gmail.com>
2021-12-17 12:49:44 -0300

    calculos de job

--- src/routes.js
+++ src/routes.js
@@ -3,39 +3,74 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
-const profile = {
-  name: "Jakeliny",
-  avatar:
-    "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
-  "monthly-budget": 3000,
-  "hours-per-day": 5,
-  "days-per-week": 5,
-  "vacation-per-year": 4,
-  "value-per-hour": 75,
+const Profile = {
+  data: {
+    name: "Jakeliny",
+    avatar:
+      "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+    "monthly-budget": 3000,
+    "hours-per-day": 5,
+    "days-per-week": 5,
+    "vacation-per-year": 4,
+    "value-per-hour": 75,
+  },
+  controllers: {
+    index(req, res) {
+      return res.render(views + "profile", { profile: Profile.data });
+    },
+  },
 };
 
-const jobs = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    "daily-hours": 2,
-    "total-hours": 60,
-    created_at: Date.now(),
-    budget: 4500,
-    remaining_hours: 3,
-    status: "progress",
+const Job = {
+  data: [
+    {
+      id: 1,
+      name: "Pizzaria Guloso",
+      "daily-hours": 2,
+      "total-hours": 60,
+      created_at: Date.now(),
+      budget: 4500,
+      remaining_hours: 3,
+    },
+    {
+      id: 2,
+      name: "OneTwo Project2",
+      "daily-hours": 3,
+      "total-hours": 2,
+      created_at: Date.now(),
+      budget: 4500,
+      remaining_hours: 3,
+    },
+  ],
+  controllers: {
+    index(req, res) {
+      const updatedJobs = Job.data.map((job) => {
+        const remaining = remainingDays(job);
+        const status = remaining <= 0 ? "done" : "progress";
+
+        return {
+          ...job,
+          remaining,
+          status,
+          budget: Profile.data["value-per-hour"] * job["total-hours"],
+        };
+      });
+
+      return res.render(views + "index", { jobs: updatedJobs });
+    },
+
+    create(req, res) {
+      const job = req.body;
+      const lastId = Job.data[Job.data.length - 1]?.id || 0;
+      job.id = lastId + 1;
+      job.created_at = Date.now();
+
+      Job.data.push(job);
+      //console.log(jobs);
+      return res.redirect("/");
+    },
   },
-  {
-    id: 2,
-    name: "OneTwo Project",
-    "daily-hours": 3,
-    "total-hours": 47,
-    created_at: Date.now(),
-    budget: 4500,
-    remaining_hours: 3,
-    status: "done",
-  },
-];
+};
 
 function remainingDays(job) {
   const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
@@ -51,38 +86,13 @@ function remainingDays(job) {
   return dayDiff;
 }
 
-routes.get("/", (req, res) => {
-  const updatedJobs = jobs.map((job) => {
-    const remaining = remainingDays(job);
-    const status = remaining <= 0 ? "done" : "progress";
-
-    return {
-      ...job,
-      remaining,
-      status,
-      budget: profile["value-per-hour"] * job["total-hours"],
-    };
-  });
-
-  return res.render(views + "index", { jobs: updatedJobs });
-});
+routes.get("/", Job.controllers.index);
 
 routes.get("/job", (req, res) => res.render(views + "job"));
-routes.post("/job", (req, res) => {
-  const job = req.body;
-  const lastId = jobs[jobs.length - 1]?.id || 0;
-  job.id = lastId + 1;
-  job.created_at = Date.now();
-
-  jobs.push(job);
-  //console.log(jobs);
-  return res.redirect("/");
-});
+routes.post("/job", Job.controllers.create);
 
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
-routes.get("/profile", (req, res) =>
-  res.render(views + "profile", { profile })
-);
+routes.get("/profile", Profile.controllers.index);
 
 routes.get("/index", (req, res) => {
   return res.redirect("/");
